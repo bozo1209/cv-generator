3c5b7ed11bcd98d7c9047aad551eff8cc9ab6582
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-05-27 06:54:06 +0200

    add lombok annotation to user entity #29

--- cv-generator-data/src/main/java/com/cvgenerator/model/User.java
+++ cv-generator-data/src/main/java/com/cvgenerator/model/User.java
@@ -3,7 +3,12 @@ package com.cvgenerator.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "app_user")
 public class User extends BaseEntity{
@@ -13,4 +18,11 @@ public class User extends BaseEntity{
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    @Builder
+    public User(Long id, String username, String password) {
+        super(id);
+        this.username = username;
+        this.password = password;
+    }
 }
