9243d920372ec81feb3e8d050329186c0e4863dc
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-06-24 21:56:59 +0200

    create experience repository #46

---
+++ cv-generator-data/src/main/java/com/cvgenerator/repository/ExperienceRepository.java
@@ -0,0 +1,7 @@
+package com.cvgenerator.repository;
+
+import com.cvgenerator.model.Experience;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+}
