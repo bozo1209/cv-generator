628747f8b5b695e950432a4e7a442412bf676b25
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-06-24 21:58:05 +0200

    create interest repository #47

---
+++ cv-generator-data/src/main/java/com/cvgenerator/repository/InterestRepository.java
@@ -0,0 +1,7 @@
+package com.cvgenerator.repository;
+
+import com.cvgenerator.model.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+}
